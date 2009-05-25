82a700aed4b069079d8a622563165b33da225ace
tuxilinx <tuxilinx@33a7eb43-6648-0410-933e-cf740565ad39>
2009-05-25 21:16:00 +0000

    Added stubs of the script, xml and config manager objects - WIP

---
+++ branches/tuxilinx/dbuild/units_forms/modules/uwinffxmlmanager.pas
@@ -0,0 +1,23 @@
+{
+   This unit is part of the WinFF project
+   Copyright (c) 2006 - 2009 Matthew Weatherford
+   http://www.winff.org
+   Licensed under the GNU GPL v3
+
+   Implements the WinFF Preset manager object and
+   WinFF configuration manager object
+}
+
+unit uwinffxmlmanager;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils; 
+
+implementation
+
+end.
+
