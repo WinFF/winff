d4a04721fd9f4c76311ec8ce6ba1f8f61606f1ed
tuxilinx <tuxilinx@33a7eb43-6648-0410-933e-cf740565ad39>
2009-05-25 21:16:00 +0000

    Added stubs of the script, xml and config manager objects - WIP

---
+++ dbuild/units_forms/modules/uwinffmpegscriptmanager.pas
@@ -0,0 +1,22 @@
+{
+   This unit is part of the WinFF project
+   Copyright (c) 2006 - 2009 Matthew Weatherford
+   http://www.winff.org
+   Licensed under the GNU GPL v3
+
+   Implements the script creator and executer object
+}
+
+unit uwinffmpegscriptmanager;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils; 
+
+implementation
+
+end.
+
