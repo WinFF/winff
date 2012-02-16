3738341e390f95a8a0e1f47da9e0b46de1a95f5e
poipodeclown@hotmail.com <poipodeclown@hotmail.com@33a7eb43-6648-0410-933e-cf740565ad39>
2012-02-16 20:02:25 +0000

    [winff-1.4] Update winff version and date in "About" window
    + Add Launchpad contributors as group to the "Thanks" list

--- branches/winff-1.4/unit3.pas
+++ branches/winff-1.4/unit3.pas
@@ -118,9 +118,10 @@ begin
     listbox1.items.add('Göran Hannel');
     listbox1.items.add('大宝(DaBao)');
     listbox1.items.add('Alexey Osipov');
+    listbox1.items.add('Launchpad contributors');
 
-    frmAbout.Label1.Caption := StringReplace(frmAbout.Label1.Caption, '%s', '1.4.1',[]);
-    frmAbout.Label2.Caption := StringReplace(frmAbout.Label2.Caption, '%s', '2006-2011',[]);
+    frmAbout.Label1.Caption := StringReplace(frmAbout.Label1.Caption, '%s', '1.4.2',[]);
+    frmAbout.Label2.Caption := StringReplace(frmAbout.Label2.Caption, '%s', '2006-2012',[]);
 
 end;
 
