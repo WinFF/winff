a99c2316f6bc4390ee470f7d52749fed9b1facb2
poipodeclown@hotmail.com <poipodeclown@hotmail.com@33a7eb43-6648-0410-933e-cf740565ad39>
2012-03-01 21:07:20 +0000

    [trunk] Update version number in trunk to 1.5.0 and update date of copyright
    - Add launchpad contributors to thanks list

--- trunk/ winff --username bggmtt/unit3.pas
+++ trunk/ winff --username bggmtt/unit3.pas
@@ -118,9 +118,10 @@ begin
     listbox1.items.add('Göran Hannel');
     listbox1.items.add('大宝(DaBao)');
     listbox1.items.add('Alexey Osipov');
+    listbox1.items.add('Launchpad contributors');
 
-    frmAbout.Label1.Caption := StringReplace(frmAbout.Label1.Caption, '%s', '1.4.0',[]);
-    frmAbout.Label2.Caption := StringReplace(frmAbout.Label2.Caption, '%s', '2006-2011',[]);
+    frmAbout.Label1.Caption := StringReplace(frmAbout.Label1.Caption, '%s', '1.5.0',[]);
+    frmAbout.Label2.Caption := StringReplace(frmAbout.Label2.Caption, '%s', '2006-2012',[]);
 
 end;
 
