ec228a2105bf8636339a898d4e92fdce44577255
poipodeclown <poipodeclown@hotmail.com>
2012-06-11 20:15:46 +0000

    [trunk] Update release script to handle winff.en.po and winff.pot properly and update repository path

--- winff/scripts/create_tar_from_svn.sh
+++ winff/scripts/create_tar_from_svn.sh
@@ -14,17 +14,18 @@ VERSION=${VERSION%.0*}
 
 SVN="/usr/bin/svn"
 TAR="/bin/tar"
-REPOSITORY="http://winff.googlecode.com/svn/trunk/%20winff%20--username%20bggmtt"
+REPOSITORY="http://winff.googlecode.com/svn/trunk/winff"
 TMPDIR=`/bin/mktemp -d -t winff.XXXXXX` || exit 1
 
 cd $TMPDIR
 pwd
 $SVN export $REPOSITORY winff
 cd winff
-rm -rf debian win32setup scripts
+rm -rf scripts
 # winff.po file is not used by the package, but is up-to-date. Create winff.pot file out of it.
 cd languages
-mv winff.po winff.pot
+cp winff.po winff.pot
+mv winff.po winff.en.po
 cd $TMPDIR
 $TAR -czf winff-${VERSION}-source.tar.gz winff
 echo "Source file can be found at ${TMPDIR}/winff-${VERSION}-source.tar.gz"
