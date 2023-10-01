0ce0de48221f005621d48339e3c9eab9e0130337
PeterB <peter@pblackman.plus.com>
2023-10-01 21:17:34 +0100

    Fix build on Unix/BSD

--- winff/unit1.pas
+++ winff/unit1.pas
@@ -28,8 +28,10 @@ uses
   {$IFDEF unix} baseunix, unix, {$endif}
   laz_xmlcfg, dom, xmlread, xmlwrite, StdCtrls, Buttons, ActnList, Menus, unit2, unit3,
   unit4, unit5, gettext, translations, process
-  {$IFDEF TRANSLATESTRING}, DefaultTranslator{$ENDIF}, ExtCtrls, ComCtrls, Spin, UTF8Process,
-  PoTranslator, types, FileUtil, regexpr, LazUTF8, LazFileUtils;
+  {$IFDEF TRANSLATESTRING}, DefaultTranslator{$ENDIF},
+  ExtCtrls, ComCtrls, Spin, UTF8Process, PoTranslator, types, FileUtil, regexpr
+  {$IFDEF WINDOWS}, LazUTF8, LazFileUtils {$endif}
+  {$IFDEF linux},   LazUTF8, LazFileUtils {$endif};
 
 type
 
@@ -1241,13 +1243,13 @@ end;
 
 // launch browser
 procedure TfrmMain.launchbrowser(URL:string);
-{$IFDEF linux}
+{$IFDEF unix}
 var
 launcher:tprocess;
 s:string;
 {$endif}
 begin
-  {$ifdef linux}
+  {$ifdef unix}
   s:='';
   if fileexists('/usr/bin/konqueror') then s:='/usr/bin/konqueror';
   if fileexists('/usr/bin/mozilla-firefox') then s:='/usr/bin/mozilla-firefox';
@@ -1382,13 +1384,13 @@ end;
 
 // launch pdf
 procedure TfrmMain.LaunchPdf(pdffile:string);
-{$IFDEF linux}
+{$IFDEF unix}
 var
 launcher:tprocess;
 s:string;
 {$endif}
 begin
-  {$ifdef linux}
+  {$ifdef unix}
   s:='';
   if fileexists('/usr/bin/okular') then s:='/usr/bin/okular';
   if fileexists('/usr/bin/evince') then s:='/usr/bin/evince';
@@ -1984,7 +1986,7 @@ var s : string;
 language: string;
 begin
   language:=leftstr(lang,2);
-  {$ifdef linux}
+  {$ifdef unix}
   s :='';
   if fileexists('/usr/share/doc/winff/WinFF.' + language + '.pdf.gz') then s:='/usr/share/doc/winff/WinFF.' + language + '.pdf.gz';
   if fileexists('/usr/share/doc/winff/WinFF.' + language + '.pdf') then s:='/usr/share/doc/winff/WinFF.' + language + '.pdf';
